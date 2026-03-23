76dcd52c71c5500b1d598c5d7c495d9ce29c3ced
--replace-all <loretodoris68@gmail.com>
2026-03-23 11:07:19 +0100

    feat/menu_header modified JS,CSS y HTML

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const formulario = document.getElementById('userForm');
 const mensajeDiv = document.getElementById('mensaje');
 
-// Escuchamos el evento "submit" (cuando se envía el formulario)
-formulario.addEventListener('submit', (event) => {
-    // Evitamos que la página se refresque
+// Escuchar datos 
+formulario.addEventListener('submit', event => {
     event.preventDefault();
 
-    // Capturamos los valores de los inputs
+    // Se añade los valores de los inputs
     const nombre = document.getElementById('nombre').value;
     const email = document.getElementById('email').value;
     const edad = document.getElementById('edad').value;
 
-    // Mostramos los datos en la consola (F12 en el navegador)
+    // Mostramos los datos en la consola 
     console.log("Datos recibidos:", { nombre, email, edad });
+    //if (edad >= 18)
 
     // Mostramos un mensaje de éxito en la página
     mensajeDiv.innerText = `¡Gracias ${nombre}! Tus datos han sido registrados.`;
